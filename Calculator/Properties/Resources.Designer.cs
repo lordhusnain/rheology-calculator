bdafb2128655e84a096ee0c71c1f66961f2b8cc2
lordhusnain <lordhusnain@gmail.com>
2024-04-23 21:13:09 +0400

    changes

--- Calculator/Properties/Resources.Designer.cs
+++ Calculator/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Calculator.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Pic {
+            get {
+                object obj = ResourceManager.GetObject("Pic", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
